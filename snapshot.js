cf30de0a1c68ac0f44575958b41051a624c3a3f9
Rahul Sudev <rsudev@allegion.com>
2025-06-04 09:41:52 -0600

    Added Percy Visual testing

---
+++ snapshot.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const percySnapshot = require('@percy/puppeteer');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, async () => {
+    console.log(`Example app listening on port ${port}`);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${port}`, { waitUntil: 'networkidle0' });
+
+    // Take a Percy snapshot
+    await percySnapshot(page, 'Home Page');
+
+    await browser.close();
+    process.exit();
+});
